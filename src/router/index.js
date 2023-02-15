3c68b02937753fd9fd0e4a4c5c12d3cea6dff90c
chihaicheng <chihaicheng@dip-ai.com>
2023-02-15 18:34:03 +0800

    init: 初始化

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+// import { lazy } from 'react';
+// import { Navigate } from 'react-router-dom';
+
+const router = [
+  {
+    path: '/',
+    element: <>1231321</>,
+    // children: []
+  },
+];
+
+export default router;
