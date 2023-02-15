005d4f935e5cee3de471718b3ec3e427023fb54a
chihaicheng <chihaicheng@dip-ai.com>
2023-02-15 18:39:34 +0800

    init 初始化

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
@@ -6,6 +6,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <Suspense
+      fallback={<>loading....</>}
+    >
+      <App />
+    </Suspense>
   </Router>
 );
