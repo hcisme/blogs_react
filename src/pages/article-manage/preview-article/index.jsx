0e892742d2594daa39c515cc2734d71150776038
chihaicheng <chc2075983104@163.com>
2023-02-23 02:03:27 +0800

    feat: 增加文章点赞功能

--- src/pages/article-manage/preview-article/index.jsx
+++ src/pages/article-manage/preview-article/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Avatar, List, message, Space, Tag } from 'antd';
-import { EyeOutlined, MessageOutlined } from '@ant-design/icons';
+import { Avatar, List, message, Space, Tag, Tooltip } from 'antd';
+import { EyeOutlined, MessageOutlined, LikeOutlined } from '@ant-design/icons';
 import { useParams } from 'react-router-dom';
 import dayjs from 'dayjs';
 import { useRequest } from 'ahooks';
@@ -8,12 +8,15 @@ import { useRequest } from 'ahooks';
 // import 'react-quill/dist/quill.core.css';
 // import 'react-quill/dist/quill.bubble.css';
 import 'quill-emoji/dist/quill-emoji.css';
+import useMessage from '../../../hooks/useMessage';
 import { getArticleInfoById } from '../../../services/articles';
+import { isStarFn } from '../../../services/star';
 import CodeHighLight from '../../../component/CodeHighLight';
 import { tagsColorList } from '../../../utils/dictionary';
+import { getLocalStorage } from '../../../utils/localStorage';
 
-const IconText = ({ icon, text }) => (
-  <Space>
+const IconText = ({ icon, text, style = {}, ...rest }) => (
+  <Space style={style} {...rest}>
     {React.createElement(icon)}
     {text}
   </Space>
@@ -21,14 +24,33 @@ const IconText = ({ icon, text }) => (
 
 function Index() {
   const { id } = useParams();
+  const messagePro = useMessage();
+  const { _id } = getLocalStorage('userInfo');
   const color = tagsColorList[Math.floor(Math.random() * tagsColorList.length)];
-  const { loading, data = {} } = useRequest(async () => {
+  const {
+    loading,
+    data = {},
+    runAsync: getArticleInfoRunAsync
+  } = useRequest(async () => {
     const { data = {}, success } = await getArticleInfoById({ id });
     if (success) {
       return data.data;
     }
     return message.error(data.message);
   });
+
+  const starFn = async ({ isStared, starId }) => {
+    const response = await isStarFn({ aid: id, starId, isStar: isStared ? 0 : 1 });
+    const { data: { message } = {} } = response;
+    messagePro({
+      response,
+      errorText: message,
+      successText: message,
+      onSuccess: () => {
+        getArticleInfoRunAsync();
+      }
+    });
+  };
 
   return (
     <List
@@ -39,11 +61,27 @@ function Index() {
       dataSource={[data]}
       footer={<span>最后更新时间：{dayjs(data.updatedAt).format('YYYY-MM-DD HH:mm')}</span>}
       renderItem={(item) => {
+        const isStared = !!item?.starList?.find((i) => i.star_user_id === _id)?.isStar;
+        const starId = item?.starList?.find((i) => i.star_user_id === _id)?._id;
         return (
           <List.Item
             key={item._id}
             actions={[
               <IconText icon={EyeOutlined} text={item.views} key="preview" />,
+              <Tooltip title="点赞👍">
+                <IconText
+                  icon={LikeOutlined}
+                  text={item?.starList?.length}
+                  key="like"
+                  style={{
+                    cursor: 'pointer',
+                    color: isStared ? 'red' : ''
+                  }}
+                  onClick={() => {
+                    starFn({ isStared, starId });
+                  }}
+                />
+              </Tooltip>,
               <IconText
                 icon={MessageOutlined}
                 text={item?.commentTotal?.length}
---
+++ src/services/star.js
@@ -0,0 +1,5 @@
+import axios from '../utils/axiosInterceptors';
+
+export const isStarFn = (params) => {
+  return axios.post('/api/star', params);
+};
